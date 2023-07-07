7e7281f33af5c1ff6b2bf27d89af0aa4582beb13
Abhishek <abhishekbidhan11@gmail.com>
2023-07-07 13:37:10 +0530

    backend class 4

---
+++ studynotion/controllers/Section.js
@@ -0,0 +1,110 @@
+const Section = require('../models/Section');
+const Course  = require('../models/Course');
+
+exports.createSection = async (req, res) => {
+    try{
+        //data fetch
+        const {sectionName, courseId} = req.body;
+
+        //data validation
+
+        if(!sectionName || !courseId){
+            return res.status(400).json({
+                success: false,
+                message: "Missing properties",
+            });
+        }
+
+        //create section
+
+        const newSection = await Section.create({sectionName});
+
+        //update coures with section ObjectID
+
+        const updatedCourseDetails = await Course.findByIdAndUpdate(
+                                                            courseId,
+                                                            {
+                                                                $push: {
+                                                                    courseContent: newSection._id,
+                                                                }
+                                                            },
+                                                            {new: true},
+                                                            );
+
+        // return res
+
+        return res.status(200).json({
+            success: true,
+            message: "Section created successfully",
+            updatedCourseDetails,
+        });
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Unable to create section.PLease try again",
+            error: error.message,
+        });
+    }
+};
+
+exports.updateSection = async (req, res) => {
+    try{
+        //data inout
+
+        const {sectionName, sectionId} = req.body;
+
+        //data validation
+
+        if(!sectionName || !sectionId){
+            return res.status(400).json({
+                success: false,
+                message: "Missing properties",
+            });
+        }
+
+        //update data
+
+        const section = await Section.findByIdAndUpdate(sectionId,{sectionName}, {new: true});
+
+        //return res
+        return res.status(200).json({
+            success: true,
+            message: "Section Updated successfully",
+        });
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Unable to update section. PLease try again",
+            error: error.message,
+        });
+    }
+};
+
+exports.deleteSection = async (req, res) => {
+    try {
+        //get ID
+
+        const {sectionId} = req.params
+
+        //delete section
+
+        await Section.findByIdAndDelete(sectionId);
+
+
+        //return res
+        return res.status(200).json({
+            success: true,
+            message: "Section Deleted successfully",
+            updatedCourseDetails,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Unable to delete section. PLease try again",
+            error: error.message,
+        });
+
+    }
+};
---
+++ studynotion/controllers/Subsection.js
@@ -0,0 +1,59 @@
+const SubSection = require("../models/SubSection");
+const Section = require("../models/Section");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+
+exports.createSubSection = async (req, res) => {
+    try {
+        //fetch data
+        const {sectionId, title, timeDuration, description} = req.body;
+
+        //extract file/video
+
+        const video = req.files.videoFile;
+        
+        //validation
+
+        if(!title || !sectionId || !timeDuration || !description){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            });
+        }
+
+        //upload video
+
+        const uploadDetails = await uploadImageToCloudinary(video, process.env.FOLDER_NAME);
+
+        //create a sub section
+
+        const subSectionDetails = await SubSection.create({
+            title: title,
+            timeDuration: timeDuration,
+            description: description,
+            videoUrl:  uploadDetails.secure_url,
+        });
+
+        //update section with sub section ID
+
+        const updatedSection = await Section.findByIdAndUpdate({_id: sectionId},
+                                                                {
+                                                                $push: {
+                                                                    subSection: subSectionDetails._id,
+                                                                    }   
+                                                                },{new: true});
+
+        //return res
+        return res.status(200).json({
+            success: true,
+            message: "SubSection created successfully",
+            updatedSection
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Unable to create subsection. PLease try again",
+            error: error.message,
+        });
+    }
+};
